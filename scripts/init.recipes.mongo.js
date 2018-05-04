98f5df71da906e8e98685b11bd278744505497c1
JackYang3567 <13808013567@163.com>
2018-05-04 15:54:20 +0800

    all 2

--- scripts/init.recipes.mongo.js
+++ scripts/init.recipes.mongo.js
@@ -1,43 +1,3 @@
-
-# trwer（hello-world 项目的第二版)
-
- 增加MongoDB ,react-router-dom。实现MERN全栈Web应用
-
-
-## Usage
-### Install MongoDB 
-Depending on your environment, the steps to install MongoDB are different.Please baidu
-
-### Install  Mongoose
-npm install --save mongoose
-
-### Install react-router-dom
-npm install --save react-router-dom
-
-## Config
-Create a directory config in the project root directory, and create a file credentials. Config.js, which contains the following code:
-
-<code>
-module.exports = {
-
-    mongo: {   
-        development: {
-        // connectionString: 'mongodb://root:12345abc@localhost:27017/admin',
-            connectionString: 'mongodb://192.168.3.10:27017/issuetracker',
-        },
-
-        production: {
-            //connectionString: 'mongodb://root:12345abc@localhost:27017/admin',
-            connectionString: 'mongodb://192.168.3.10:27017/issuetracker',
-        },
-    },
-};
-</code>
-
-## Initialization data
-- Create a directory scripts in the project root directory, and create a file init.recipes.mongo.js, which contains the following code:
-
-<code>
 db = new Mongo().getDB('issuetracker');
 
 db.recipes.remove({});
@@ -81,17 +41,3 @@ db.recipes.insert([
 );
 
 db.recipes.createIndex({ name: 1 });
-</code>
-
-- Run on the command line: mongo scripts/init.recipes.mongo.js
-## CRUD
-
-
-## The routing configuration
-
-
-
-## REST APIs
-
-
-
